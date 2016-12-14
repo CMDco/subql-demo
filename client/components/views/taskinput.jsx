aca778c4c2280d3c1f5576ce377ec293fb5b89d0
Martin-Ting <martinhting@gmail.com>
2016-12-13 22:02:58 -0800

    chore: fixed file structure and server

---
+++ client/components/views/taskinput.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TaskInput extends Component {
+  constructor(props) {
+
+  }
+  render() {
+    return (
+      <input type={'text'} placeholder={'Enter a task.'}> </input>
+    )
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
+const path = require('path');
+
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+
 const app = express();
 const server = require('http').Server(app);
+
 const { buildSchema } = require('graphql');
 // const { parseSchema, registerResolver, registerType, getRoot } = require('../src/subql.js');
 
+app.use(express.static(path.join(__dirname + '/../dist/')));
+
 app.get('/', function (req, res) {
-  res.send("Hello, World");
+  res.redirect('/index.html');
 });
 
-server.listen(8080);
+server.listen(8080, () => {
+  console.log('Server started on port 8080');
+});
