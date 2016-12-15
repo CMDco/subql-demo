a317cad13678834354dae959da47cd78b405f0b5
Dean Edwards <dmedwards4@gmail.com>
2016-12-15 11:26:50 -0800

    feat: add postgres tables in sequelize for taskmanger demo

--- server/db/models/tables.js
+++ server/db/models/tables.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 const { POSTGRES_USERNAME, POSTGRES_PASSWORD } = require('../config.secret');
 
-const sequelize = new Sequelize('', POSTGRES_USERNAME, POSTGRES_PASSWORD, {
+const sequelize = new Sequelize('taskmanager', POSTGRES_USERNAME, POSTGRES_PASSWORD, {
   dialect: 'postgres',
   port: 5432,
 });
@@ -16,13 +16,7 @@ sequelize.authenticate()
     console.log('Unable to connect to database',err);
   });
 
-//models
 const User = sequelize.define('user', {
-  _id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
   username: {
     type: Sequelize.STRING,
     allowNull: false
@@ -30,50 +24,155 @@ const User = sequelize.define('user', {
 });
 
 const Tasklist = sequelize.define('tasklist', {
-  _id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
   },
+  position: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
 });
 
 const Task = sequelize.define('task', {
-  _id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
+  author: {
+    type: Sequelize.STRING,
+    allowNull: false
   },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  label: {
+    type: Sequelize.STRING,
+    allowNull: true
+  }
 });
 
 const Activity = sequelize.define('activity', {
-  _id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
+  author: {
+    type: Sequelize.STRING,
+    allowNull: false
   },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
 });
 
 const Board = sequelize.define('board', {
-  _id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: true
+  }
 });
 
 const Comment = sequelize.define('comment', {
-  _id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
+  author: {
+    type: Sequelize.STRING,
+    allowNull: false
   },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+    allowNull: false
+  }
 });
 
 //Associations between Tables
-Board.hasMany(User, {as: 'users'});
+Board.hasMany(User, { as: 'user', foreignKey: 'boardID' });
+Board.hasMany(Activity, { as: 'activity', foreignKey: 'boardID' });
+Board.hasMany(Tasklist, { as: 'tasklist', foreignKey: 'boardID' });
+
+Tasklist.hasMany(Task, { as: 'task', foreignKey: 'tasklistID' });
+
+Task.hasMany(Comment, { foreignKey: 'taskID' });
 
 //force tables to drop each time file is run,.
-sequelize.sync({ /*logging: console.log, force: true */}).then(() => {  });
+sequelize.sync({ logging: console.log, force: true }).then(() => {  
+  Board.create({
+    title: 'New Board'
+  }).then((result) => {
+    Tasklist.create({
+      title: 'What needs to be done',
+      position: 0,
+      boardID: result.dataValues.id
+    }).then((result) => {
+      Task.create({
+        author: 'rasec',
+        content: 'Demo-app to test SubQL',
+        tasklistID: result.dataValues.id
+      }).then((result) => {
+        Comment.create({
+          author: 'rasec',
+          content: 'The demo-app should be a task manager',
+          taskID: result.dataValues.id
+        });
+      });
+      Task.create({
+        author: 'rasec',
+        content: 'Website for SubQL',
+        tasklistID: result.dataValues.id
+      }).then((result) => {
+        Comment.create({
+          author: 'rasec',
+          content: 'The website should use flexbox',
+          taskID: result.dataValues.id
+        });
+        Comment.create({
+          author: 'rasec',
+          content: 'The website should have a minimalist style',
+          taskID: result.dataValues.id
+        });
+      }); 
+    });
+    
+    Tasklist.create({
+      title: 'What we have completed',
+      position: 1,
+      boardID: result.dataValues.id
+    }).then((result) => {
+      Task.create({
+        author: 'rasec',
+        content: 'Basic functionality of SubQL',
+        tasklistID: result.dataValues.id
+      }).then((result) => {
+        Comment.create({
+          author: 'rasec',
+          content: 'Good Job guys!',
+          taskID: result.dataValues.id
+        });
+      });
+      Task.create({
+        author: 'rasec',
+        content: 'Publish on NPM',
+        tasklistID: result.dataValues.id
+      }).then((result) => {
+        Comment.create({
+          author: 'rasec',
+          content: 'We need to bolster our docs',
+          taskID: result.dataValues.id
+        });
+        Comment.create({
+          author: 'rasec',
+          content: 'We need to also get ready for launch',
+          taskID: result.dataValues.id
+        });
+      });
+    });
+  });
+
+  return;
+});
 
 //export tables to be used in get request files.
 module.exports =  { User, Tasklist, Task, Activity, Board, Comment };
