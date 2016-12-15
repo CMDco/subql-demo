7a4856c2f2c53be77adbd4c9d0cafebc6b2db196
cesarhernar <cesarh@usc.edu>
2016-12-14 19:21:34 -0800

    feat: add activity feed components

---
+++ client/components/containers/activityFeed.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Activity from './../views/Activity.jsx';
+
+class ActivityFeed extends Component{ 
+  constructor(props) { 
+    super(props);
+  }
+  render() {
+      let activities = [];
+      this.props.activFeed.forEach(activity => { 
+          activities.push(<Activity time={activity.time} content={activity.content} author={activity.author}/>)
+      })
+    return (
+      <div>
+            {activities}
+      </div>
+    )
+  }
+};
+
+
+export default ActivityFeed;
